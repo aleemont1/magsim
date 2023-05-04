41d3be3c7f66b131ef35914396af555505cfca7c
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2023-05-04 20:02:02 +0200

    [PLOT] Create a good 2D plot on xz plane

--- coilBuilder.py
+++ coilBuilder.py
@@ -1,14 +1,22 @@
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import magpylib as magpy
 import pyvista as pv
+
+pl = pv.Plotter(
+    multi_samples=8,
+    lighting='none'
+)
 
 avv = int(input("Numero di avvolgimenti:"))
 l1 = int(input("Lato 1:"))
 l2 = int(input("Lato 2:"))
 h = float(input("Altezza bobina:"))
 i = float(input("Corrente:"))
-sensor = magpy.Sensor()
+
+sensor = magpy.Sensor(
+    position=(0, l2/2, h/2)
+    )
 coil = magpy.Collection()
 vertices=(
             (-l1/2,0,0),
@@ -26,46 +34,37 @@ for z in np.linspace(0, h, avv):
         position = (0,0,z)
     )
     coil.add(winding)
-magpy.show(sensor, coil, backend='plotly')
 
-grid = pv.UniformGrid(
-    dimensions=(l1, l1, l1),
-    spacing=(2, 2, 2),
-)
-
-grid['B'] = coil.getB(grid.points)
-
-seed = pv.Disc(inner=1, outer=5.2, r_res=3, c_res=12)
-strl = grid.streamlines_from_source(
-    seed,
-    vectors='B',
-    max_time=180,
-    initial_step_length=0.01,
-    integration_direction='both',
-)
-
-# create plotting scene
-pl = pv.Plotter()
-
-# add field lines and legend to scene
-legend_args = {
-    'title': 'B [mT]',
-    'title_font_size': 20,
-    'color': 'black',
-    'position_y': 0.25,
-    'vertical': True,
-}
-
-# draw coils
-magpy.show(coil, canvas=pl, backend='pyvista')
-
-# add streamlines
-pl.add_mesh(
-    strl.tube(radius=.2),
-    cmap="bwr",
-    scalar_bar_args=legend_args,
-)
-# display scene
-pl.camera.position=(160, 10, -10)
-pl.set_background("white")
+magpy.show(coil, backend='pyvista', canvas=pl)
 pl.show()
+
+# compute step size based on input data
+max_dim = max(l1, l2, h)
+min_dim = min(l1, l2, h)
+step_size = 0.01 * max_dim / (max_dim // 100)
+if step_size > 0.5 * min_dim:
+    step_size = 0.5 * min_dim
+
+# create grid
+grid_dim = int(max_dim * 2)
+ts = np.arange(-grid_dim, grid_dim + step_size, step_size)
+grid = np.array([[(x,0,z) for x in ts] for z in ts])
+
+# calculate B-field [mT]
+B = magpy.getB(sources=coil, observers=grid)
+print(B)
+
+Bamp = np.linalg.norm(B, axis=2)
+Bamp /= np.amax(Bamp)
+
+sp = plt.streamplot(
+    grid[:,:,0], grid[:,:,2], B[:,:,0], B[:,:,2],
+    density=2,
+    color=Bamp,
+    linewidth=np.sqrt(Bamp)*3,
+    cmap='coolwarm'
+)
+
+plt.colorbar(sp.lines, label='[mT]')
+plt.tight_layout()
+plt.show()
